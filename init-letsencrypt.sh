87e6f3b2d96a8f2eca3581f8dd528e7f2e54aa81
Bulat Gilfanov <bulatgilfanov@gmail.com>
2025-03-16 21:44:33 +0300

    init

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -3,7 +3,6 @@
 # Конфигурация
 domain="api.xn--80aaekaddf2agvt2a6b8c3ckw.xn--p1ai"
 email="your-email@example.com"    # Замените на свой email
-staging=1                         # Установите в 0 для боевого сертификата
 rsa_key_size=4096
 
 # Проверка root прав
@@ -21,8 +20,8 @@ mkdir -p certbot/www
 echo "Останавливаем контейнеры..."
 docker-compose -f docker-compose.prod.yml down
 
-echo "Запускаем только nginx и web..."
-docker-compose -f docker-compose.prod.yml up -d nginx web
+echo "Запускаем только nginx..."
+docker-compose -f docker-compose.prod.yml up -d nginx
 echo "Ждем запуск nginx..."
 sleep 10
 
@@ -31,16 +30,9 @@ echo "Проверяем доступность домена..."
 curl -I http://$domain
 
 echo "Запускаем certbot..."
-if [ $staging != "0" ]; then
-    staging_arg="--staging"
-else
-    staging_arg=""
-fi
-
 docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
     certbot certonly --webroot \
     -w /var/www/certbot \
-    $staging_arg \
     --email $email \
     -d $domain \
     --rsa-key-size $rsa_key_size \
@@ -56,6 +48,4 @@ docker-compose -f docker-compose.prod.yml up -d
 echo "Проверяем статус nginx..."
 docker-compose -f docker-compose.prod.yml exec nginx nginx -t
 
-echo "Готово! Проверьте работу сайта по адресу https://$domain"
-echo "Если вы использовали staging=1, то браузер покажет предупреждение о сертификате"
-echo "Для получения боевого сертификата установите staging=0 и запустите скрипт снова" 
+echo "Готово! Проверьте работу сайта по адресу https://$domain" 
