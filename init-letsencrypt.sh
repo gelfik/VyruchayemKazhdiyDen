e83f20e2bad3a635374e8746fe243c73fb0de592
Bulat Gilfanov <bulatgilfanov@gmail.com>
2025-03-16 14:38:50 +0300

    init

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Конфигурация
-domain="api.выручаемкаждыйдень.рф"
+domain_ru="api.выручаемкаждыйдень.рф"
+domain="api.xn--80aaekaddf2agvt2a6b8c3ckw.xn--p1ai"
 email="your-email@example.com"    # Замените на свой email
 staging=1                         # Установите в 0 для боевого сертификата
 rsa_key_size=4096
@@ -14,14 +15,14 @@ fi
 
 # Создаем необходимые директории
 echo "Создаем директории для certbot..."
-mkdir -p certbot/conf/live/$domain
+mkdir -p certbot/conf/live/$domain_ru
 mkdir -p certbot/www
 
 # Создаем временный самоподписанный сертификат
 echo "Создаем временный сертификат..."
 openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1 \
-    -keyout certbot/conf/live/$domain/privkey.pem \
-    -out certbot/conf/live/$domain/fullchain.pem \
+    -keyout certbot/conf/live/$domain_ru/privkey.pem \
+    -out certbot/conf/live/$domain_ru/fullchain.pem \
     -subj '/CN=localhost'
 
 # Остановка существующих контейнеров
@@ -35,7 +36,7 @@ sleep 5
 
 # Удаляем временный сертификат
 echo "Удаляем временный сертификат..."
-rm -rf certbot/conf/live/$domain/*
+rm -rf certbot/conf/live/$domain_ru/*
 
 # Запрашиваем сертификат
 echo "Запрашиваем сертификат Let's Encrypt..."
@@ -47,7 +48,7 @@ fi
 
 # Проверяем доступность домена
 echo "Проверяем доступность домена..."
-curl -I http://$domain
+curl -I http://$domain_ru
 
 echo "Запускаем certbot..."
 docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
@@ -69,6 +70,6 @@ docker-compose -f docker-compose.prod.yml up -d
 echo "Проверяем статус сертификата..."
 docker-compose -f docker-compose.prod.yml exec nginx nginx -t
 
-echo "Готово! Проверьте работу сайта по адресу https://$domain"
+echo "Готово! Проверьте работу сайта по адресу https://$domain_ru"
 echo "Если вы использовали staging=1, то браузер покажет предупреждение о сертификате"
 echo "Для получения боевого сертификата установите staging=0 и запустите скрипт снова" 
