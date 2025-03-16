850cbea11fc7e6de3d0ae4fd95efb15bd03c036c
Bulat Gilfanov <bulatgilfanov@gmail.com>
2025-03-16 14:10:39 +0300

    init

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -3,22 +3,39 @@
 domain="api.xn--80aafkpbcdcb3agfghez2axe1c.xn--p1ai"
 email="your-email@example.com"  # Замените на свой email
 
+# Остановим контейнеры если они запущены
+docker-compose -f docker-compose.prod.yml down
+
 # Создаем необходимые директории
-mkdir -p "./certbot/conf/live/$domain"
-mkdir -p "./certbot/www"
+mkdir -p certbot/conf certbot/www
 
 # Запускаем nginx
-docker-compose -f docker-compose.prod.yml up --force-recreate -d nginx
+docker-compose -f docker-compose.prod.yml up -d nginx
 
-# Запрашиваем сертификат
-docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
+# Ждем запуска nginx
+echo "Waiting for nginx to start..."
+sleep 10
+
+# Запрашиваем сертификат в режиме staging (тестовый режим)
+docker-compose -f docker-compose.prod.yml run --rm certbot certbot certonly \
+    --webroot \
+    --webroot-path /var/www/certbot \
     --email $email \
-    -d $domain \
-    --rsa-key-size 4096 \
     --agree-tos \
-    --force-renewal \
-    --cert-name $domain" certbot
+    --no-eff-email \
+    --staging \
+    -d $domain
 
-# Перезапускаем nginx
-docker-compose -f docker-compose.prod.yml exec nginx nginx -s reload 
+# Если тестовый сертификат получен успешно, запрашиваем боевой
+echo "Requesting production certificate..."
+docker-compose -f docker-compose.prod.yml run --rm certbot certbot certonly \
+    --webroot \
+    --webroot-path /var/www/certbot \
+    --email $email \
+    --agree-tos \
+    --no-eff-email \
+    --force-renewal \
+    -d $domain
+
+# Перезапускаем nginx для применения сертификата
+docker-compose -f docker-compose.prod.yml restart nginx 
